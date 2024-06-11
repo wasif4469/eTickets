7a97797d1e6258dc33a915074a244e0ce496e900
wasif4469 <wasifnaziree@gmail.com>
2024-06-11 18:05:03 +0500

    done

--- obj/Debug/net6.0/eTickets.AssemblyInfo.cs
+++ obj/Debug/net6.0/eTickets.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15bfe0a9747fa9e49d085715a89e321963b3b92b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
